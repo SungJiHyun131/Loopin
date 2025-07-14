4ac7992e141a8d1ec6a31ef29e2b5ba789d38461
SungJiHyun131 <jihyun131@naver.com>
2025-07-14 11:23:36 +0900

    메인 홈 변경, 아티스트 미디어 배경 변경, 로고 추가

--- project01/src/pages/MainHome.jsx
+++ project01/src/pages/MainHome.jsx
@@ -2,16 +2,22 @@ import React,{useState} from 'react'
 import ArtistCard from '../components/ArtistCard'
 import './MainHome.css'
 import SearchBar from '../components/SearchBar';
-import heart from'../assets/img/heart.png'
 import filter from'../assets/img/filter_icon.png'
 const MainHome = () => {
   const [query, setQuery] = useState('');
 
   return (
     <div className='container main'>
-      <p className="txt1">심장 저격할 최애 찾기 <img src={heart} alt="" /></p>
+     
        <SearchBar onChange={(e) => setQuery(e.target.value)} />
-      <p className="filter"><img src={filter} alt="" /></p>
+       {/* <p className="filter"><img src={filter} alt="" /></p> */}
+       <p className="txt1">
+        나의 아티스트를 선택해주세요
+       </p>
+       <p className="txt2">
+        Please select your artist
+       </p>
+     
      <ArtistCard/>
     </div>
   )
