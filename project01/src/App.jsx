72267a73c662eb3c905e7476d9bae49f5cd38eed
SungJiHyun131 <jihyun131@naver.com>
2025-07-15 16:05:07 +0900

    펜페이지 변경, 새로운 페이지 추가 ...등등 ..

--- project01/src/App.jsx
+++ project01/src/App.jsx
@@ -29,6 +29,8 @@ import IllitHome from './pages/artist/illit/IllitHome';
 import FanHome from './pages/artist/fan/FanHome';
 import FanPostDetail from './pages/artist/fan/FanPostDetail';
 import FanPostWrite from './pages/artist/fan/FanPostWrite';
+import FanLetter from './pages/artist/fan/FanLetter';
+import SecretLetter from './pages/artist/fan/SecretLetter';
 import LiveHome from './pages/artist/live/LiveHome';
 import LiveDetail from './pages/artist/live/LiveDetail';
 
@@ -86,7 +88,8 @@ function App() {
   <Route path="FanPostDetail" element={<FanPostDetail/>} />
   <Route path="FanPostWrite" element={<FanPostWrite/>} />
   </Route>
-
+  <Route path="FanLetter" element={<FanLetter/>} />
+  <Route path="SecretLetter" element={<SecretLetter/>} />
 </Routes>
 
   );
---
+++ project01/src/pages/artist/fan/FanLetter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FanLetterHeader from '../../../components/FanLetterHeader';
+const FanLetter = () => {
+  return (
+    <div>
+      <div className="FanLetterHeader">
+          <FanLetterHeader />
+        </div>
+    </div>
+  )
+}
+
+export default FanLetter
