a3f6b076a40663af88c131f353076da557a0bbd7
Hyeri-Yeom <yeomhy02@gmail.com>
2025-07-16 15:47:09 +0900

    챗봇수정

--- project01/src/components/ChatbotModal.jsx
+++ project01/src/components/ChatbotModal.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './ChatbotModal.css';
 import {Link, useLocation, Route} from 'react-router-dom';
 import close from '../assets/img/header_close.png';
-
+import talk from '../assets/img/chatbot-talkicon.png';
+import ask from '../assets/img/chatbot-ask.png';
+import profile from '../../public/loopin.svg'
 const ChatbotModal = ({ onClose }) => {
-  const location =useLocation();
+  const location = useLocation();
   return (
     <div className="chatbot-modal-overlay" onClick={onClose}>
       <div className="chatbot-modal" onClick={(e) => e.stopPropagation()}>
@@ -14,6 +16,7 @@ const ChatbotModal = ({ onClose }) => {
           <div className="chatbot-profile">
             <p className="profileImg"></p>
             <div className="rightText">
+            <p className="img"><img src={profile} alt="" /></p>
               <p className="name">Loopin</p>
               <span>운영시간 보기</span>
             </div>
@@ -25,12 +28,17 @@ const ChatbotModal = ({ onClose }) => {
 
         <div className="chatbot-body">
           <div className="chatbot-conBox">
-            <p className="profileImg"></p>
-            <p>안녕하세요 LOOPIN입니다 💙</p>
-            <button className="askbtn"><Link to='/ChatbotView'>문의하기<img src="" alt="" /></Link></button>
+            <div className="profile">
+              <p className="img"><img src={profile} alt="" /></p>
+              <div className="profile-name">
+                <p className='name'>LOOPIN</p>
+                <p className='intro'>안녕하세요 <br/>LOOPIN입니다 💙</p>
+              </div>
+            </div>
+            <button className="askbtn"><Link to='/ChatbotView'>문의하기<img src={ask} alt="" /></Link></button>
             <p className="smallText">몇 분 내 답변 받으실 수 있어요</p>
           </div>
-          <p className="using"><img src="" alt="" />채널톡 이용중</p>
+          <p className="using"><img src={talk} alt="" style={{ width:"15px"}}/>채널톡 이용중</p>
         </div>
       </div>
     </div>
