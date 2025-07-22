fc2a0e6de27a69afc7d4937e94d8a6a88f364ad4
Hyeri-Yeom <yeomhy02@gmail.com>
2025-07-22 11:53:29 +0900

    뭔가바꿈

--- project01/src/pages/artist/media/MediaHome.jsx
+++ project01/src/pages/artist/media/MediaHome.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import MediaHeader from '../../../components/MediaHeader';
 import './Media.css';
-
+import headerbg from '../../../assets/img/IllitHomeimg/mediaimg/bgheader-media.png';
+import bg from '../../../assets/img/IllitHomeimg/mediaimg/bg-media.png';
 
 const MediaHome = () => {
   const location = useLocation();
@@ -13,6 +14,8 @@ const MediaHome = () => {
   };
   return (
     <div className='MediaContainer'>
+      <div className="Media-headerbg">
+        <img src={headerbg} alt="" className='mediaheaderbg'/>
       <div className="MediaHeader">
         <MediaHeader/>
       </div>
@@ -21,9 +24,10 @@ const MediaHome = () => {
   right: getIndex() === 0 ? `calc(0%-1px)` : undefined,}} />
       <Link to="MediaA" className="toggle-btn">홈</Link>
       <Link to="MediaB" className="toggle-btn">전체</Link>
-     
+      </div>
     </div>
     <Outlet />
+    <div className="Media-bg"><img src={bg} alt=""/></div> 
     </div>
   );
 };
